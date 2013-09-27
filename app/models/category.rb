bc4ceb7411b55d05d75c2e954e29d4a4155689d5
Shawn Harkness <shawnhark@gmail.com>
2013-09-26 21:32:22 -0400

    Comment vote fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-	has_many :post_categories
-	has_many :posts, through: :post_categories
+	has_many :postcategories
+	has_many :posts, through: :postcategories
 
 	validates :name, presence: true
 
